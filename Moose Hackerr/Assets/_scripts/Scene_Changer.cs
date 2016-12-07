b5ea9a6cbb25624e1a8a4ad801500b45e32743d2
DominicSears <dominicsears@hotmail.com>
2016-12-07 08:44:14 -0800

    Switch scenes, new scene

---
+++ Moose Hackerr/Assets/_scripts/Scene_Changer.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using UnityEditor.SceneManagement;
+
+public class Scene_Changer : MonoBehaviour {
+
+    void switch_scene(string nextScene, string switchFrom)
+    {
+        SceneManager.LoadScene(nextScene);
+        Debug.Log (nextScene + ": scene loaded");
+        //EditorSceneManager.OpenScene(SceneManager.GetSceneByName(nextScene).path);
+        SceneManager.UnloadScene(SceneManager.GetSceneByName(switchFrom));
+        Debug.Log (switchFrom + ": scene unloaded");
+    }
+
+	// Use this for initialization
+	void Start ()
+    {
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (ReloadLevel.hackStarted)
+        {
+            //Debug.Log ("Scene_Changer: hackStarted is true");
+            //SceneManager.LoadScene("hack");
+            //Debug.Log ("Scene_Changer: Scene changed to \"hack\"");
+            //EditorSceneManager.OpenScene(SceneManager.GetSceneByName("hack").path);
+            //SceneManager.UnloadScene(SceneManager.GetSceneByName("room1"));
+
+            switch_scene ("hack", "room1");
+        }
+    }
+}
